478847a46efc5847ac5aa71ec3ef4a0d920154da
SergiOn <onishhenko@gmail.com>
2017-10-15 18:54:19 +0300

    I Scopes And Digest # Value-Based Dirty-Checking

--- src/scope.js
+++ src/scope.js
@@ -8,14 +8,23 @@ function Scope() {
     this.$$lastDirtyWatch = null;
 }
 
-Scope.prototype.$watch = function (watchFn, listenerFn) {
+Scope.prototype.$watch = function (watchFn, listenerFn, valueEq) {
     var watcher = {
         watchFn: watchFn,
         listenerFn: listenerFn || function () { },
+        valueEq: !!valueEq,
         last: initWatchVal
     };
     this.$$watchers.push(watcher);
     this.$$lastDirtyWatch = null;
+};
+
+Scope.prototype.$$areEqual = function (newValue, oldValue, valueEq) {
+    if (valueEq) {
+        return _.isEqual(newValue, oldValue);
+    } else {
+        return newValue === oldValue;
+    }
 };
 
 Scope.prototype.$$digestOnce = function () {
@@ -25,9 +34,9 @@ Scope.prototype.$$digestOnce = function () {
     _.forEach(this.$$watchers, function (watcher) {
         newValue = watcher.watchFn(self);
         oldValue = watcher.last;
-        if (newValue !== oldValue) {
+        if (!self.$$areEqual(newValue, oldValue, watcher.valueEq)) {
             self.$$lastDirtyWatch = watcher;
-            watcher.last = newValue;
+            watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
             watcher.listenerFn(
                 newValue,
                 oldValue === initWatchVal ? newValue : oldValue,
