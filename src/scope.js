eb9c701dc4f8580a2c13adad85444e0b79350547
SergiOn <onishhenko@gmail.com>
2017-10-15 18:18:51 +0300

    I Scopes And Digest # Short-Circuiting The Digest When The Last Watch Is Clean

--- src/scope.js
+++ src/scope.js
@@ -5,6 +5,7 @@ function initWatchVal() { }
 
 function Scope() {
     this.$$watchers = [];
+    this.$$lastDirtyWatch = null;
 }
 
 Scope.prototype.$watch = function (watchFn, listenerFn) {
@@ -14,6 +15,7 @@ Scope.prototype.$watch = function (watchFn, listenerFn) {
         last: initWatchVal
     };
     this.$$watchers.push(watcher);
+    this.$$lastDirtyWatch = null;
 };
 
 Scope.prototype.$$digestOnce = function () {
@@ -24,6 +26,7 @@ Scope.prototype.$$digestOnce = function () {
         newValue = watcher.watchFn(self);
         oldValue = watcher.last;
         if (newValue !== oldValue) {
+            self.$$lastDirtyWatch = watcher;
             watcher.last = newValue;
             watcher.listenerFn(
                 newValue,
@@ -31,6 +34,8 @@ Scope.prototype.$$digestOnce = function () {
                 self
             );
             dirty = true;
+        } else if (self.$$lastDirtyWatch === watcher) {
+            return false;
         }
     });
     return dirty;
@@ -39,6 +44,7 @@ Scope.prototype.$$digestOnce = function () {
 Scope.prototype.$digest = function () {
     var dirty = false;
     var ttl = 10;
+    this.$$lastDirtyWatch = null;
     do {
         dirty = this.$$digestOnce();
         if (dirty && !(ttl--)) {
