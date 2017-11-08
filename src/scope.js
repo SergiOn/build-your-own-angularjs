5adaa3428df7bb4345dcdf96e81402edd7314ca6
SergiOn <onishhenko@gmail.com>
2017-11-08 23:22:37 +0200

    I Scope Events # Broadcasting Down The Scope Hierarchy

--- src/scope.js
+++ src/scope.js
@@ -405,6 +405,9 @@ Scope.prototype.$emit = function (eventName) {
 Scope.prototype.$broadcast = function (eventName) {
     var event = {name: eventName};
     var listenerArgs = [event].concat(_.tail(arguments));
-    this.$$fireEventOnScope(eventName, listenerArgs);
+    this.$$everyScope(function (scope) {
+        scope.$$fireEventOnScope(eventName, listenerArgs);
+        return true;
+    });
     return event;
 };
