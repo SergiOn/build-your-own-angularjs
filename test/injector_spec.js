6cac4de76898bab15b0e898c7d3ea172e08a5581
SergiOn <onishhenko@gmail.com>
2017-12-07 01:35:35 +0200

    III (Modules And Dependency Injection) Modules And The Injector # Registering A Constant

--- test/injector_spec.js
+++ test/injector_spec.js
@@ -1,5 +1,5 @@
 /* jshint globalstrict: true */
-/* global createInjectot: false, setupModuleLoader: false, angular: false */
+/* global createInjector: false, setupModuleLoader: false, angular: false */
 'use strict';
 
 describe('injector', function () {
@@ -12,6 +12,34 @@ describe('injector', function () {
     it('can be created', function () {
         var injector = createInjector([]);
         expect(injector).toBeDefined();
+    });
+
+    it('has a constant that been registered to a module', function () {
+        var module = angular.module('myModule', []);
+        module.constant('aConstant', 42);
+        var injector = createInjector(['myModule']);
+        expect(injector.has('aConstant')).toBe(true);
+    });
+
+    it('does not have a non-registered constant', function () {
+        var module = angular.module('myModule', []);
+        var injector = createInjector(['myModule']);
+        expect(injector.has('aConstant')).toBe(false);
+    });
+
+    it('does not allow a constant called hasOwnProperty', function () {
+        var module = angular.module('myModule', []);
+        module.constant('hasOwnProperty', _.constant(false));
+        expect(function () {
+            createInjector(['myModule']);
+        }).toThrow();
+    });
+
+    it('can return a registered constant', function () {
+        var module = angular.module('myModule', []);
+        module.constant('aConstant', 42);
+        var injector = createInjector(['myModule']);
+        expect(injector.get('aConstant')).toBe(42);
     });
 
 
