b833bb1df161ba7f6caa1dafbc47bf61df8d42c9
SergiOn <onishhenko@gmail.com>
2017-12-21 13:56:53 +0200

    IV (Utilities) Promises # Resolving A Deferred

--- test/q_spec.js
+++ test/q_spec.js
@@ -4,11 +4,13 @@
 
 describe('$q', function () {
 
-    var $q;
+    var $q, $rootScope;
 
     beforeEach(function () {
         publishExternalAPI();
-        $q = createInjector(['ng']).get('$q');
+        var injector = createInjector(['ng']);
+        $q = injector.get('$q');
+        $rootScope = injector.get('$rootScope');
     });
 
     it('can create a Deferred', function () {
@@ -20,5 +22,59 @@ describe('$q', function () {
         var d = $q.defer();
         expect(d.promise).toBeDefined();
     });
+
+    it('can resolve a promise', function (done) {
+        var deferred = $q.defer();
+        var promise = deferred.promise;
+
+        var promiseSpy = jasmine.createSpy();
+        promise.then(promiseSpy);
+
+        deferred.resolve('a-ok');
+
+        setTimeout(function () {
+            expect(promiseSpy).toHaveBeenCalledWith('a-ok');
+            done();
+        }, 1);
+    });
+
+    it('works when resolved before promise listener', function (done) {
+        var d = $q.defer();
+        d.resolve(42);
+
+        var promiseSpy = jasmine.createSpy();
+        d.promise.then(promiseSpy);
+
+
+        setTimeout(function () {
+            expect(promiseSpy).toHaveBeenCalledWith(42);
+            done();
+        }, 0);
+    });
+
+    it('does not resolve promise immediately', function () {
+        var d = $q.defer();
+
+        var promiseSpy = jasmine.createSpy();
+        d.promise.then(promiseSpy);
+
+        d.resolve(42);
+
+        expect(promiseSpy).not.toHaveBeenCalled();
+    });
+
+    it('resolve promise at next digest', function () {
+        var d = $q.defer();
+
+        var promiseSpy = jasmine.createSpy();
+        d.promise.then(promiseSpy);
+
+        d.resolve(42);
+        $rootScope.$apply();
+
+        expect(promiseSpy).toHaveBeenCalledWith(42);
+    });
+
+
 
 });
