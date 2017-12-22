84a38394afa302cdaf51a90b12964127efc11f97
SergiOn <onishhenko@gmail.com>
2017-12-22 22:45:13 +0200

    IV (Utilities) Promises # Registering Multiple Promise Callbacks

--- src/q.js
+++ src/q.js
@@ -9,7 +9,8 @@ function $QProvider() {
             this.$$state = {};
         }
         Promise.prototype.then = function (onFulfilled) {
-            this.$$state.pending = onFulfilled;
+            this.$$state.pending = this.$$state.pending || [];
+            this.$$state.pending.push(onFulfilled);
             if (this.$$state.status > 0) {
                 scheduleProcessQueue(this.$$state);
             }
@@ -38,7 +39,11 @@ function $QProvider() {
         }
 
         function processQueue(state) {
-            state.pending(state.value);
+            var pending = state.pending;
+            delete state.pending;
+            _.forEach(pending, function (onFulfilled) {
+                onFulfilled(state.value);
+            });
         }
 
         return {
