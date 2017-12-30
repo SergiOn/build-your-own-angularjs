d6260d36c5d413819660b89a303f24cc83fb6e4c
SergiOn <onishhenko@gmail.com>
2017-12-30 23:21:02 +0200

    IV (Utilities) $http # Response Transforms

--- src/http.js
+++ src/http.js
@@ -54,35 +54,54 @@ function $HttpProvider() {
         }, headers);
     }
 
+    function isSuccess(status) {
+        return status >= 200 && status < 300;
+    }
+
+    function headersGetter(headers) {
+        var headersObj;
+        return function (name) {
+            headersObj = headersObj || parseHeaders(headers);
+            if (name) {
+                return headersObj[name.toLowerCase()];
+            } else {
+                return headersObj;
+            }
+        };
+    }
+
+    function parseHeaders(headers) {
+        if (_.isObject(headers)) {
+            return _.transform(headers, function (result, v, k) {
+                result[_.trim(k.toLowerCase())] = _.trim(v);
+            }, {});
+        } else {
+            var lines = headers.split('\n');
+            return _.transform(lines, function (result, line) {
+                var separatorAt = line.indexOf(':');
+                var name = _.trim(line.substr(0, separatorAt)).toLowerCase();
+                var value = _.trim(line.substr(separatorAt + 1));
+                if (name) {
+                    result[name] = value;
+                }
+            }, {});
+        }
+    }
+
+    function transformData(data, headers, status, transform) {
+        if (_.isFunction(transform)) {
+            return transform(data, headers, status);
+        } else {
+            return _.reduce(transform, function (data, fn) {
+                return fn(data, headers, status);
+            }, data);
+        }
+    }
+
     this.$get = ['$httpBackend', '$q', '$rootScope', function ($httpBackend, $q, $rootScope) {
 
-        function $http(requestConfig) {
+        function sendReq(config, reqData) {
             var deferred = $q.defer();
-
-            var config = _.extend({
-                method: 'GET',
-                transformRequest: defaults.transformRequest
-            }, requestConfig);
-            config.headers = mergeHeaders(requestConfig);
-
-            if (_.isUndefined(config.withCredentials) &&
-                !_.isUndefined(defaults.withCredentials)) {
-                config.withCredentials = defaults.withCredentials;
-            }
-
-            var reqData = transformData(
-                config.data,
-                headersGetter(config.headers),
-                config.transformRequest
-            );
-
-            if (_.isUndefined(reqData)) {
-                _.forEach(config.headers, function (v, k) {
-                    if (k.toLowerCase() === 'content-type') {
-                        delete config.headers[k];
-                    }
-                });
-            }
 
             function done(status, response, headersString, statusText) {
                 status = Math.max(status, 0);
@@ -98,50 +117,6 @@ function $HttpProvider() {
                 }
             }
 
-            function isSuccess(status) {
-                return status >= 200 && status < 300;
-            }
-
-            function headersGetter(headers) {
-                var headersObj;
-                return function (name) {
-                    headersObj = headersObj || parseHeaders(headers);
-                    if (name) {
-                        return headersObj[name.toLowerCase()];
-                    } else {
-                        return headersObj;
-                    }
-                };
-            }
-
-            function parseHeaders(headers) {
-                if (_.isObject(headers)) {
-                    return _.transform(headers, function (result, v, k) {
-                        result[_.trim(k.toLowerCase())] = _.trim(v);
-                    }, {});
-                } else {
-                    var lines = headers.split('\n');
-                    return _.transform(lines, function (result, line) {
-                        var separatorAt = line.indexOf(':');
-                        var name = _.trim(line.substr(0, separatorAt)).toLowerCase();
-                        var value = _.trim(line.substr(separatorAt + 1));
-                        if (name) {
-                            result[name] = value;
-                        }
-                    }, {});
-                }
-            }
-
-            function transformData(data, headers, transform) {
-                if (_.isFunction(transform)) {
-                    return transform(data, headers);
-                } else {
-                    return _.reduce(transform, function (data, fn) {
-                        return fn(data, headers);
-                    }, data);
-                }
-            }
-
             $httpBackend(
                 config.method,
                 config.url,
@@ -152,6 +127,55 @@ function $HttpProvider() {
             );
             return deferred.promise;
         }
+
+        function $http(requestConfig) {
+            var config = _.extend({
+                method: 'GET',
+                transformRequest: defaults.transformRequest,
+                transformResponse: defaults.transformResponse
+            }, requestConfig);
+            config.headers = mergeHeaders(requestConfig);
+
+            if (_.isUndefined(config.withCredentials) &&
+                !_.isUndefined(defaults.withCredentials)) {
+                config.withCredentials = defaults.withCredentials;
+            }
+
+            var reqData = transformData(
+                config.data,
+                headersGetter(config.headers),
+                undefined,
+                config.transformRequest
+            );
+
+            if (_.isUndefined(reqData)) {
+                _.forEach(config.headers, function (v, k) {
+                    if (k.toLowerCase() === 'content-type') {
+                        delete config.headers[k];
+                    }
+                });
+            }
+
+            function transformResponse(response) {
+                if (response.data) {
+                    response.data = transformData(
+                        response.data,
+                        response.headers,
+                        response.status,
+                        config.transformResponse
+                    );
+                }
+                if (isSuccess(response.status)) {
+                    return response;
+                } else {
+                    return $q.reject(response);
+                }
+            }
+
+            return sendReq(config, reqData)
+                .then(transformResponse, transformResponse);
+        }
+
         $http.defaults = defaults;
         return $http;
     }];
