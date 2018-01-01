306bf87fd4534e8f71a60227d22ccd5d2f46f2ce
SergiOn <onishhenko@gmail.com>
2018-01-02 00:11:42 +0200

    V (Directives) DOM Compilation and Basic Directives # Registering Directives

--- karma.conf.js
+++ karma.conf.js
@@ -31,6 +31,7 @@ module.exports = function(config) {
       // 'test/**/http_spec.js'
       // 'test/**/filter_spec.js'
       // 'test/**/filter_filter_spec.js'
+      'test/**/compile_spec.js'
     ],
 
 
