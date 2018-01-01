6af374bd9162c449e9d5652fd6d02f4f8363af43
SergiOn <onishhenko@gmail.com>
2018-01-01 20:18:31 +0200

    IV (Utilities) $http # Request Timeouts

--- src/http.js
+++ src/http.js
@@ -180,6 +180,7 @@ function $HttpProvider() {
                 reqData,
                 done,
                 config.headers,
+                config.timeout,
                 config.withCredentials
             );
             return deferred.promise;
