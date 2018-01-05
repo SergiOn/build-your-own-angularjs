cac0b2224d1d039e2d02df373af4bff6acddc74e
SergiOn <onishhenko@gmail.com>
2018-01-06 01:09:21 +0200

    V (Directives) Directive Attributes # Introducing A Test Helper

--- test/compile_spec.js
+++ test/compile_spec.js
@@ -2,15 +2,14 @@
 /* global publishExternalAPI: false, createInjector: false */
 'use strict';
 
-function makeInjectorWithDirectives() {
-    var args = arguments;
-    return createInjector(['ng', function ($compileProvider) {
-        $compileProvider.directive.apply($compileProvider, args);
-    }]);
-}
-
-
 describe('$compile', function () {
+
+    function makeInjectorWithDirectives() {
+        var args = arguments;
+        return createInjector(['ng', function ($compileProvider) {
+            $compileProvider.directive.apply($compileProvider, args);
+        }]);
+    }
 
     beforeEach(function () {
         delete window.angular;
@@ -651,40 +650,44 @@ describe('$compile', function () {
 
     describe('attributes', function () {
 
-        it('pass the element attributes to the compile function', function () {
-            var injector = makeInjectorWithDirectives('myDirective', function () {
+        function registerAndCompile(dirName, domString, callback) {
+            var givenAttrs;
+            var injector = makeInjectorWithDirectives(dirName, function () {
                 return {
-                    restrict: 'E',
+                    restrict: 'EACM',
                     compile: function (element, attrs) {
-                        element.data('givenAttrs', attrs);
+                        givenAttrs = attrs;
                     }
                 };
             });
             injector.invoke(function ($compile) {
-                var el = $('<my-directive my-attr="1" my-other-attr="two"></my-directive>');
+                var el = $(domString);
                 $compile(el);
-
-                expect(el.data('givenAttrs').myAttr).toBe('1');
-                expect(el.data('givenAttrs').myOtherAttr).toBe('two');
+                callback(el, givenAttrs);
             });
+        }
+
+        it('pass the element attributes to the compile function', function () {
+            registerAndCompile(
+                'myDirective',
+                '<my-directive my-attr="1" my-other-attr="two"></my-directive>',
+                function (element, attrs) {
+                    expect(attrs.myAttr).toBe('1');
+                    expect(attrs.myOtherAttr).toBe('two');
+                }
+            );
         });
 
         it('trims attribute values', function () {
-            var injector = makeInjectorWithDirectives('myDirective', function () {
-                return {
-                    restrict: 'E',
-                    compile: function (element, attrs) {
-                        element.data('givenAttrs', attrs);
-                    }
-                };
-            });
-            injector.invoke(function ($compile) {
-                var el = $('<my-directive my-attr=" val "></my-directive>');
-                $compile(el);
-
-                expect(el.data('givenAttrs').myAttr).toBe('val');
-            });
+            registerAndCompile(
+                'myDirective',
+                '<my-directive my-attr=" val "></my-directive>',
+                function (element, attrs) {
+                    expect(attrs.myAttr).toBe('val');
+                }
+            );
         });
+
 
 
     });
