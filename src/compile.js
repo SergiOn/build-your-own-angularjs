45128ba562d6717aed33c6c5e3679913de76c604
SergiOn <onishhenko@gmail.com>
2018-01-14 18:13:55 +0200

    V (Directives) Controllers # Self-Requiring Directives

--- src/compile.js
+++ src/compile.js
@@ -102,6 +102,7 @@ function $CompileProvider($provide) {
                         directive.$$bindings = parseDirectiveBindings(directive);
                         directive.name = directive.name || name;
                         directive.index = i;
+                        directive.require = directive.require || (directive.controller && name);
                         return directive;
                     });
                 }]);
