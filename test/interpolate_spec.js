0f4713bceccc45a8559ac773b8a82547e808b3a7
SergiOn <onishhenko@gmail.com>
2018-01-25 00:12:48 +0200

    V (Directives) Interpolation # Interpolating Strings

--- test/interpolate_spec.js
+++ test/interpolate_spec.js
@@ -14,4 +14,39 @@ describe('$interpolate', function () {
         expect(injector.has('$interpolate')).toBe(true);
     });
 
+    it('produces an identity function for static content', function () {
+        var injector = createInjector(['ng']);
+        var $interpolate = injector.get('$interpolate');
+
+        var inter = $interpolate('hello');
+        expect(inter instanceof Function).toBe(true);
+        expect(inter()).toBe('hello');
+    });
+
+    it('evaluates a single expression', function () {
+        var injector = createInjector(['ng']);
+        var $interpolate = injector.get('$interpolate');
+
+        var inter = $interpolate('{{anAttr}}');
+        expect(inter({anAttr: '42'})).toBe('42');
+    });
+
+    it('evaluates many expressions', function () {
+        var injector = createInjector(['ng']);
+        var $interpolate = injector.get('$interpolate');
+
+        var inter = $interpolate('First {{anAttr}}, then {{anotherAttr}}!');
+        expect(inter({anAttr: '42', anotherAttr: '43'})).toBe('First 42, then 43!');
+    });
+
+    it('passes through ill-defined interpolations', function () {
+        var injector = createInjector(['ng']);
+        var $interpolate = injector.get('$interpolate');
+
+        var inter = $interpolate('why u no }}work{{');
+        expect(inter({})).toBe('why u no }}work{{');
+    });
+
+
+
 });
